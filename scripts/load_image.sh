7217c0d2a2ed5338aa06c56bed4e9a3efd87655c
hanshengng <82213205+hanshengng@users.noreply.github.com>
2021-04-14 01:30:30 +0800

    Update load_image.sh

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-echo "test"
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 669189415304.dkr.ecr.ap-southeast-1.amazonaws.com
+
+docker pull 669189415304.dkr.ecr.ap-southeast-1.amazonaws.com/one2onerepo:latest
+
+docker run --env DATA_FILE="Questions-test.json" 669189415304.dkr.ecr.ap-southeast-1.amazonaws.com/one2onerepo:latest
